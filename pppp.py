f731b3788a998748429f67a79766a364d1bab8a5
Tom Ritchford <tom@swirly.com>
2019-05-26 17:28:56 +0200

    Better commands and errors

--- pppp.py
+++ pppp.py
@@ -2,29 +2,6 @@
 Keeps a permanent stack of current project directories
 """
 
-"""
-Keep a stack of current projects - directories, essentially.
-
-* go to
-  * current project
-  * project n
-
-* rotate [default +1]
-  * rotating by 1 means putting the current project at the back
-
-* push a new project
-  * at the top
-  * at position n
-
-* pop
-  * the current project
-  * project n
-
-* undo the last operation
-
-* list projects
-"""
-
 import inspect
 import json
 import os
@@ -33,6 +10,7 @@ import sys
 CONFIG_ENV = '__PPPP_CONFIG'
 DEFAULT_CONFIG = '~/.pppp.json'
 CONFIG_FILE = os.path.expanduser(os.getenv(CONFIG_ENV, DEFAULT_CONFIG))
+DEFAULT_COMMAND = 'goto'
 
 
 def pppp(*args):
@@ -40,15 +18,35 @@ def pppp(*args):
     for a in args:
         (commands, hlp)[a in ('--help', '-h')].append(a)
 
-    command = commands and commands.pop(0)
-    if command and (command.startswith('_') or not hasattr(Projects, command)):
-        _perr('Do not understand command', command)
+    command = commands and commands.pop(0) or ''
 
-    elif hlp:
+    if command.startswith('_'):
+        _perr('Do not understand command', command)
+        return
+
+    if command.isnumeric():
+        commands = command, *commands
+        command = 'goto'
+
+    if command:
+        for m in dir(Projects):
+            if m.startswith(command):
+                command = m
+                break
+        else:
+            _perr('Do not understand command', command)
+            return
+
+    if hlp:
         _help(command)
 
     else:
-        getattr(Projects(), command)(*args)
+        try:
+            result = getattr(Projects(), command or DEFAULT_COMMAND)(*commands)
+        except Exception as e:
+            _perr('ERROR', e)
+            result = None
+        print(result or os.getcwd())
 
 
 class Projects:
@@ -61,23 +59,18 @@ class Projects:
             self.projects, self.undo = [], []
         self.original_projects = self.projects[:]
 
-    @property
-    def project(self):
-        return self.projects and self.projects[0]
-
     def goto(self, position=0):
         """Go right to a project at a specific position or by default the top
            project."""
-        return (
-            self.projects and self.projects[int(position) % len(self.projects)]
-        )
+        return self.projects and self.projects[self._to_pos(position)]
 
     def rotate(self, steps=1):
         """Rotate the list of project directories in a cycle for one or more
            steps in either direction"""
-        steps = int(steps) % len(self.projects)
+        steps = self._to_pos(steps)
         self.projects = self.projects[steps:] + self.projects[:steps]
         self._write()
+        return self.goto()
 
     def push(self, project=None, position=0):
         """Push a project directory into the project list.
@@ -91,15 +84,16 @@ class Projects:
         if project in self.projects:
             raise ValueError('Cannot insert the same project twice')
 
-        position = int(position)
-        self.projects.insert(int(position), project)
+        self.projects.insert(self._to_pos(position), project)
         self._write()
+        return self.goto()
 
     def pop(self, position=0):
         """Pop and discard a project - default 0 means the most recent one"""
         if self.projects:
-            _perr('Popped', self.projects.pop(int(position)))
+            _perr('Popped', self.projects.pop(self._to_pos(position)))
             self._write()
+            self.goto()
 
     def undo(self):
         """Undoes the previous change to the projects list"""
@@ -110,15 +104,22 @@ class Projects:
         """Clears the list of projects"""
         self.projects.clear()
         self._write()
+        self.list()
 
     def list(self):
         """Lists all the projects in order"""
+        if not self.projects:
+            _perr('(no projects)')
+
         for i, p in enumerate(self.projects):
             _perr('%d: %s' % (i, p))
 
     def _write(self):
         with open(CONFIG_FILE, 'w') as fp:
             json.dump([self.projects, self.original_projects], fp)
+
+    def _to_pos(self, pos):
+        return (int(pos) % len(self.projects)) if self.projects else 0
 
 
 
@@ -136,13 +137,8 @@ def _help(command):
             sig = inspect.signature(method)
         except Exception:
             continue
-        sig = (
-            str(sig)
-            .replace('(self', '')
-            .replace(')', '')
-            .replace(',', '')
-        ).lstrip()
-        _perr('pppp', c, sig)
+        params = [str(p) for p in sig.parameters.values()][1:]
+        _perr('pppp', c, *params)
         for line in method.__doc__.splitlines():
             _perr('   ', line.replace(11 * ' ', ''))
         _perr()
